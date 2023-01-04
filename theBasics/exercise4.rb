390469cfce5c1e90eb1ce2cc0da503dccbc473b2
Christopher Perkins <Christopher.Perkins@null.net>
2023-01-04 18:28:10 -0500

    The Basics: Exercise 4 done

---
+++ theBasics/exercise4.rb
@@ -0,0 +1,11 @@
+# Use the dates from the previous example and store them in an array. Then make
+# your program output the same thing as exercise 3.
+years = [2010, 1986, 1977, 2003]
+
+puts years[0]
+puts years[1]
+puts years[2]
+puts years[3]
+
+# Again, just like last time, this would probably be less typing with iteration.
+# We haven't be taught that yet, though, so this is what we have been taught.
