46914c95fee5381d1a2f71652dedf8c97bf4e565
Christopher Perkins <Christopher.Perkins@null.net>
2023-01-04 18:40:25 -0500

    The Basics: Exercise 6 done

---
+++ theBasics/exercise6.rb
@@ -0,0 +1,11 @@
+# Write a program that calculates the squares of 3 float numbers of your
+# choosing and outputs the result to the screen.
+first = 352.1
+second = 19.125
+third = 3.14
+
+puts first * first
+puts second * second
+puts third**2
+
+# We weren't taught the ** operator, but I'm sneaky and wanted to use it.
