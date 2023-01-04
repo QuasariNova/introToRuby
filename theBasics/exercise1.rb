1dc6e041ec2adf07dc57bf2e9af2e8d6853ed3a4
Christopher Perkins <Christopher.Perkins@null.net>
2023-01-04 18:05:17 -0500

    The Basics: Exercise 1 done

---
+++ theBasics/exercise1.rb
@@ -0,0 +1,5 @@
+# Add two strings together that, when concatenated, return your first and last
+# name as your full name in one string.
+# "<Firstname> <LastName>"
+# For example, if your name is John Doe, think about how you can put "John" and "Doe" together to get "John Doe".
+"Christopher " + "Perkins"
