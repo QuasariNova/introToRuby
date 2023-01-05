13ef25cd9d6bff197e7e0b69e732262bde0e602d
Christopher Perkins <Christopher.Perkins@null.net>
2023-01-05 15:19:45 -0500

    Methods: Exercise 3 complete

---
+++ methods/exercise3.rb
@@ -0,0 +1,10 @@
+# Write a program that includes a method called multiply that takes two a
+# rguments and returns the product of the two numbers.
+
+def multiply (a, b)
+  a * b
+end
+
+puts multiply(10, 84.3)
+
+# Since the last one didn't want user input, I didn't do it here.
