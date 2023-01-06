c5d46c1b8c6fbdfd951956dd9d836c0b70117382
Christopher Perkins <Christopher.Perkins@null.net>
2023-01-06 16:55:51 -0500

    Exercises: Exercise 8 complete

---
+++ exercises/exercise8.rb
@@ -0,0 +1,5 @@
+# Create a Hash, with one key-value pair, using both Ruby syntax styles.
+new_style_hash = {name: "Chris"}
+old_style_hash = { :name => "Chris" }
+
+p new_style_hash == old_style_hash
